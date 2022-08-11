bb70b0b8be2b12099b5930c43a020e36bad1b302
Kong ji han <wisejohn950330@gmail.com>
2022-08-11 16:57:50 +0900

    서비스 레이어 책 등록 완료

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -1,8 +1,34 @@
 package site.metacoding.junitproject.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.web.dto.BookRespDto;
+import site.metacoding.junitproject.web.dto.BookSaveReqDto;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+    
+    // 1. 책 등록
+    @Transactional(rollbackOn = RuntimeException.class)
+    public BookRespDto 책등록하기(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
+
+    // 2. 책 목록보기
+
+    // 3. 책 한건보기
+
+    // 4. 책 삭제
+
+    // 5. 책 수정
     
 }
