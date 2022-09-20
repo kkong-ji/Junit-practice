29f27b5ffa06928d7f915005a9fbd05245a8abdd
Kong ji han <wisejohn950330@gmail.com>
2022-09-20 11:34:53 +0900

    서비스 레이어 테스트 - 책 목록보기

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -36,9 +36,19 @@ public class BookService {
 
     // 2. 책 목록보기
     public List<BookRespDto> 책목록보기() {
-        return bookRepository.findAll().stream()
-            .map(new BookRespDto()::toDto)
-            .collect(Collectors.toList());
+        // 코드 수정
+        List<BookRespDto> dtos = bookRepository.findAll().stream()
+                .map((bookPS) -> new BookRespDto().toDto(bookPS))
+                .collect(Collectors.toList());
+
+        // print
+        dtos.stream().forEach((dto) -> {
+            System.out.println("================= 본코드");
+            System.out.println(dto.getId());
+            System.out.println(dto.getTitle());
+        });
+
+        return dtos;
     }
 
     // 3. 책 한건보기
--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -5,6 +5,10 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import site.metacoding.junitproject.domain.Book;
 import site.metacoding.junitproject.domain.BookRepository;
 import site.metacoding.junitproject.util.MailSender;
 import site.metacoding.junitproject.util.MailSenderStub;
@@ -49,8 +54,35 @@ public class BookServiceTest {
         // then
         // assertEquals(dto.getTitle(), bookRespDto.getTitle());
         // assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
-        assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
-        assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
+
+        assertThat(bookRespDto.getTitle()).isEqualTo(dto.getTitle());
+        assertThat(bookRespDto.getAuthor()).isEqualTo(dto.getAuthor());
     }
-    
+
+    @Test
+    public void 책목록보기_테스트() {
+        // given(파라미터로 들어올 데이터)
+
+        // stub (가설)
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1L, "junit강의", "메타코딩"));
+        books.add(new Book(2L, "spring강의", "겟인데어"));
+        when(bookRepository.findAll()).thenReturn(books);
+
+        // when (실행)
+        List<BookRespDto> bookRespDtoList = bookService.책목록보기();
+
+        // print
+        bookRespDtoList.stream().forEach((dto) -> {
+            System.out.println("================= 테스트");
+            System.out.println(dto.getId());
+            System.out.println(dto.getTitle());
+        });
+
+        // then (검증)
+        assertThat(bookRespDtoList.get(0).getTitle()).isEqualTo("junit강의");
+        assertThat(bookRespDtoList.get(0).getAuthor()).isEqualTo("메타코딩");
+        assertThat(bookRespDtoList.get(1).getTitle()).isEqualTo("spring강의");
+        assertThat(bookRespDtoList.get(1).getAuthor()).isEqualTo("겟인데어");
+    }    
 }
