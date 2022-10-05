c1225ed363fce550108d631a2c2dc23a4f346c9b
unknown <wisejohn950330@gmail.com>
2022-10-05 10:46:12 +0900

    컨트롤러 레이어 - 책 한건보기, 책 삭제하기

--- src/main/java/site/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/site/metacoding/junitproject/web/BookApiController.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,18 +66,25 @@ public class BookApiController {  // 컴포지션 = has 관계
     @GetMapping("/api/v1/book")
     public ResponseEntity<?> getBookList() {
         BookListRespDto bookList = bookService.책목록보기();
-        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 목록 가져오기 성공").body(bookList).build(),
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("책 한건보기 성공").body(bookList).build(),
                 HttpStatus.OK); // 200 = ok;
     }
 
     // 3. 책한건보기
-    public ResponseEntity<?> getBookOne() {
-        return null;
+    @GetMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> getBookOne(@PathVariable Long id) {
+        BookRespDto bookRespDto = bookService.책한건보기(id);
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 목록보기 성공").body(bookRespDto).build(),
+                HttpStatus.OK); // 200 = ok;
     }
 
+
     // 4. 책삭제하기
-    public ResponseEntity<?> deleteBook() {
-        return null;
+    @DeleteMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        bookService.책삭제하기(id);
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 삭제하기 성공").body(null).build(),
+                HttpStatus.OK); // 200 = ok;
     }
 
     // 5. 책수정하기
